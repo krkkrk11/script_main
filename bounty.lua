7cc0c7cfb8c1f5af226581848740cb996a6ae4c1
Nolife <69518147+krkkrk11@users.noreply.github.com>
2023-09-30 10:12:00 +0700

    Update bounty.lua

--- bounty.lua
+++ bounty.lua
@@ -12,4 +12,30 @@ if not game:IsLoaded() then
 	task.wait(10);
 end;
 
-loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()
+join = game.Players.localPlayer.Neutral == false
+	if (_G.Team == "Pirates" or _G.Team == "Marines") and not join then
+		repeat wait()
+			pcall(function()
+				join = game.Players.localPlayer.Neutral == false
+				if _G.Team == "Pirates" then
+					for i,v in pairs({"MouseButton1Click", "MouseButton1Down", "Activated"}) do
+						for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Pirates.Frame.ViewportFrame.TextButton[v])) do
+							v.Function()
+						end
+					end
+				elseif _G.Team == "Marines" then
+					for i,v in pairs({"MouseButton1Click", "MouseButton1Down", "Activated"}) do
+						for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Marines.Frame.ViewportFrame.TextButton[v])) do
+							v.Function()
+						end
+					end
+				else
+					for i,v in pairs({"MouseButton1Click", "MouseButton1Down", "Activated"}) do
+						for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Pirates.Frame.ViewportFrame.TextButton[v])) do
+							v.Function()
+						end
+					end
+				end
+			end)
+		until join == true and game.Players.LocalPlayer.Team ~= nil and game:IsLoaded() 
+	end
