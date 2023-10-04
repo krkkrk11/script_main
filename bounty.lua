ee3af92fa98dc723b4caeb9491572d7322eb2218
Nolife <69518147+krkkrk11@users.noreply.github.com>
2023-10-04 14:05:47 +0700

    Update bounty.lua

--- bounty.lua
+++ bounty.lua
@@ -12,4 +12,33 @@ if not game:IsLoaded() then
 	task.wait(10);
 end;
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua')){186871753943}
+getgenv().Setting = {
+    ["Start"] = true,
+    ["Team"] = "Pirates",
+    ["MasteryFarm"] = "Half",
+    ["PlayerHuntQuest"] = true,
+    ["HopwhileManyPlayer"] = false,
+    ["Ignore Hop"] = false,
+    ["Disabled3dRender"] = true,
+    ["LowGraphicQuality"] = true,
+    ["FastAttackDelay"] = "0.175",
+    ["Lock Fragments"] = 10000,
+    ["AutoBuyFruit"] = true,
+    ["ImportantFruit"] = {
+        [1] = "Dragon-Dragon",
+        [2] = "Venom-Venom",
+        [3] = "Dough-Dough",
+        [4] = "Rumble-Rumble",
+        [5] = "Soul-Soul",
+        [6] = "Human-Human: Buddha",
+        [7] = "Quake-Quake",
+        [8] = "Dark-Dark",
+        [9] = "String-String"
+    },
+    ["RBXAcc"] = {
+        ["AutoDescription"] = true,
+        ["Delay"] = 3,
+        ["Alias"] = true
+    }
+}
+loadstring(game:HttpGet'https://api.quartyz.com/script/BitHub.lua')()
