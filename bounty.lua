4673ddac3483e0c3cabb7f92bb27db6decf15247
Nolife <69518147+krkkrk11@users.noreply.github.com>
2023-10-09 18:31:00 +0700

    Update bounty.lua

--- bounty.lua
+++ bounty.lua
@@ -1,8 +1,6 @@
 repeat wait() until game:IsLoaded()
 repeat wait() until game:GetService("Players")
 repeat wait() until game:GetService("Players").LocalPlayer
-repeat wait() until game:GetService("Players").LocalPlayer.PlayerGui
-repeat wait() until game:GetService("ReplicatedStorage").Effect.Container
 
 if not game:IsLoaded() then
 	local GameLoadGui = Instance.new("Message",workspace);
